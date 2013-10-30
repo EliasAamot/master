3a699caf46734be49edcda0794c5ee8a8180fe2e
Elias <elias.aamot@gmail.no>
2013-10-30 15:41:10 +0100

    Fixed a bug in TD-IDF

--- calculate_tfidf.py
+++ calculate_tfidf.py
@@ -6,7 +6,7 @@ Created on Tue Oct 29 10:32:58 2013
 """
 import url_fetch, math, sortdict
 
-MEDLINE_SIZE = 2300000
+MEDLINE_SIZE = 23000000
 tfidfs = sortdict.SortDict()
 
 with open('results.txt', 'r') as file:
@@ -15,12 +15,12 @@ with open('results.txt', 'r') as file:
             keyword, tf = line.strip().split(':')
             df = url_fetch.get_count_for_keyword(keyword)
             if df < 2:
-                tfidfs[keyword] = "n/a"
+                tfidfs[keyword] = 0.0
             else:
-                idf = math.log((float(MEDLINE_SIZE)) / (float(df), 2))
+                idf = math.log(float(MEDLINE_SIZE) / float(df), 2)
                 tfidfs[keyword] = float(tf)*idf
-        except:
-            print "Failed to download count index for " + keyword + ", probably due to timeout."
+        except Exception as e:
+            print "Exception " + str(e) + " occurred. Skipping the keyword " + keyword + "."
 
 with open('tfidfs.txt', 'w') as file:
     for key in tfidfs[:]:
